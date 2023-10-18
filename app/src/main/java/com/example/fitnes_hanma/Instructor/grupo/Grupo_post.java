b9b765c064617e7caf576c78ed9dab373e0e582c
Nancy <nlopez45@ucol.mx>
2023-10-17 19:42:48 -0600

    modificacion de home, calendario semanal y configuraciones

---
+++ app/src/main/java/com/example/fitnes_hanma/Instructor/grupo/Grupo_post.java
@@ -0,0 +1,16 @@
+package com.example.fitnes_hanma.Instructor.grupo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.fitnes_hanma.R;
+
+public class Grupo_post extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grupo_post);
+    }
+}
