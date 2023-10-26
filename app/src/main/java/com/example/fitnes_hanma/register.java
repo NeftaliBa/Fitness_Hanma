e827ea8191ffae7960fbf90c49155965ad6c0695
Esteban <lalcaraz11@ucol.mx>
2023-10-26 09:49:34 -0600

    cambios a register y recuperar contraseña

--- app/src/main/java/com/example/fitnes_hanma/register.java
+++ app/src/main/java/com/example/fitnes_hanma/register.java
@@ -1,16 +1,17 @@
 package com.example.fitnes_hanma;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.fitnes_hanma.Cliente.principal;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -24,11 +25,11 @@ import java.util.Map;
 
 public class register extends AppCompatActivity {
 
-
     Button btn_register;
     EditText name, email, password;
     FirebaseFirestore mFirestore;
     FirebaseAuth mAuth;
+    private ProgressDialog mDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,12 +38,24 @@ public class register extends AppCompatActivity {
 
         mFirestore = FirebaseFirestore.getInstance();
         mAuth = FirebaseAuth.getInstance();
+        mDialog = new ProgressDialog(this);
 
 
         name = findViewById(R.id.nombre);
         email = findViewById(R.id.correo);
         password = findViewById(R.id.contrasena);
         btn_register = findViewById(R.id.btn_registro);
+
+        TextView textViewButton = findViewById(R.id.iniciarSesion);
+        textViewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Aquí define la lógica para redirigir a otro apartado de la aplicación
+                // Puedes usar un Intent para iniciar otra actividad, por ejemplo:
+                Intent intent = new Intent(register.this, login.class);
+                startActivity(intent);
+            }
+        });
 
         btn_register.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,6 +67,9 @@ public class register extends AppCompatActivity {
                 if (nameUser.isEmpty() && emailUser.isEmpty() && passUser.isEmpty()) {
                     Toast.makeText(register.this, "Complete los datos", Toast.LENGTH_SHORT).show();
                 } else {
+                    mDialog.setMessage("Registrando cuenta...");
+                    mDialog.setCanceledOnTouchOutside(false);
+                    mDialog.show();
                     registerUser(nameUser, emailUser, passUser);
                 }
             }
@@ -76,13 +92,15 @@ public class register extends AppCompatActivity {
                     @Override
                     public void onSuccess(Void unused) {
                         finish();
-                        startActivity(new Intent(register.this, login .class));
+                        startActivity(new Intent(register.this, login.class));
                         Toast.makeText(register.this, "Usuario registrado con éxito", Toast.LENGTH_SHORT).show();
+                        mDialog.dismiss();
                     }
                 }).addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
                         Toast.makeText(register.this, "Error al guardar", Toast.LENGTH_SHORT).show();
+                        mDialog.dismiss();
                     }
                 });
 
@@ -91,6 +109,7 @@ public class register extends AppCompatActivity {
             @Override
             public void onFailure(@NonNull Exception e) {
                 Toast.makeText(register.this, "Error al registrar", Toast.LENGTH_SHORT).show();
+                mDialog.dismiss();
             }
         });
     }
